4dfbe844cb41f6f66e22bcb29bc03aedaffc12b8
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-02-18 15:56:56 +0530

    Update InstallCitrixworkspace.ps1

--- InstallCitrixworkspace.ps1
+++ InstallCitrixworkspace.ps1
@@ -1,4 +1,5 @@
-$uri4= "https://avdappsrepository.blob.core.windows.net/appbinaries/CitrixWorkspaceApp19.12.6000.exe"
-Invoke-WebRequest -Uri '$uri4' -UseBasicParsing -OutFile "c:\buildArtifacts\CitrixWorkspaceApp.exe"
+$uri4 = "https://avdappsrepository.blob.core.windows.net/appbinaries/CitrixWorkspaceApp19.12.6000.zip"
+Invoke-WebRequest -Uri '$uri4' -UseBasicParsing -OutFile "c:\buildArtifacts\CitrixWorkspaceApp.zip"
+Expand-Archive -LiteralPath "C:\buildArtifacts\CitrixWorkspaceApp.zip" -DestinationPath "C:\buildArtifacts" -Force -Verbose
 cd "C:\buildArtifacts"
 .\CitrixWorkspaceApp.exe /AutoUpdateCheck=disabled /silent
