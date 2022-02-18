5dd322d3aedefc3183fbabd3cc02d3f22428d062
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-02-18 19:07:45 +0530

    Update InstallPowerBI.ps1

--- InstallPowerBI.ps1
+++ InstallPowerBI.ps1
@@ -3,4 +3,4 @@ Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\PBID
 
 cd "c:\buildArtifacts"
 .\PBIDesktopSetup_x64.exe -s ACCEPT_EULA=1
-Start-Sleep -Seconds 20
+Start-Sleep -Seconds 90
