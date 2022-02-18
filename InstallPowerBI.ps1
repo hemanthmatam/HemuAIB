0b87d835e7f256076537d83b7fdba391fe18fdbc
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-02-18 19:07:03 +0530

    Update InstallPowerBI.ps1

--- InstallPowerBI.ps1
+++ InstallPowerBI.ps1
@@ -1,6 +1,6 @@
 $uri1="https://avdappsrepository.blob.core.windows.net/appbinaries/PBIDesktopSetup_x64.exe"
-Invoke-WebRequest -Uri '$uri1' -UseBasicParsing -OutFile "c:\buildArtifacts\PBIDesktopSetup_x64.exe"
-Start-Sleep -Seconds 10
+Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\PBIDesktopSetup_x64.exe"
+
 cd "c:\buildArtifacts"
 .\PBIDesktopSetup_x64.exe -s ACCEPT_EULA=1
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 20
