94ac50ca25bd0229b79ea96d38e2897b774a3f93
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-02-21 19:14:51 +0530

    Update Installvisio.ps1

--- Installvisio.ps1
+++ Installvisio.ps1
@@ -2,4 +2,4 @@ $uri2="https://avdappsrepository.blob.core.windows.net/appbinaries/Visio OfficeS
 Invoke-WebRequest -Uri "$uri2" -UseBasicParsing -OutFile "c:\buildArtifacts\VisioSetup.exe"
 cd "C:\buildArtifacts"
 .\VisioSetup.exe
-Start-Sleep -Seconds 180
+Start-Sleep -Seconds 360
