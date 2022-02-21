cf7755afac0a67d2bff89d8bb9749c27c73bd2f7
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-02-21 12:15:26 +0530

    Update InstallProject.ps1

--- InstallProject.ps1
+++ InstallProject.ps1
@@ -1,6 +1,5 @@
 $uri3="https://avdappsrepository.blob.core.windows.net/appbinaries/Project OfficeSetup.exe"
 Invoke-WebRequest -Uri "$uri3" -UseBasicParsing -OutFile "c:\buildArtifacts\ProjectSetup.exe"
-
 cd "c:\buildArtifacts"
 .\ProjectSetup.exe
 Start-Sleep -Seconds 180
