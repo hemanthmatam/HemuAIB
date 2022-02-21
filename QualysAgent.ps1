f5169a846d2de2f9efe53cb274613f21875aa1ea
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-02-21 19:08:34 +0530

    Update QualysAgent.ps1

--- QualysAgent.ps1
+++ QualysAgent.ps1
@@ -1,6 +1,6 @@
 $uri="https://avdappsrepository.blob.core.windows.net/appbinaries/Qualys_CloudSecurityAgent_4.4.0.38_R1.zip"
 Invoke-WebRequest -Uri "$uri" -UseBasicParsing -OutFile "C:\buildArtifacts\QualysAgent.zip"
-Expand-Archive -LiteralPath "C:\Repos\QualysAgent.zip" -DestinationPath "C:\buildArtifacts" -Force -Verbose
+Expand-Archive -LiteralPath "C:\buildArtifacts\QualysAgent.zip" -DestinationPath "C:\buildArtifacts" -Force -Verbose
 cd C:\buildArtifacts\Qualys_CloudSecurityAgent_4.4.0.38_R1\Package\x64\
 .\Qualys_CloudSecurityAgent_x64_4.4.0.38_ALL_R1_Install.vbs
 Sleep 10
