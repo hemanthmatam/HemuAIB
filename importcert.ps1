4f4a46c1d5969ee218ff74a41e2c63099e7f0fc2
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-02 13:18:38 +0530

    Create importcert.ps1

---
+++ importcert.ps1
@@ -0,0 +1,4 @@
+$uri1="https://avdappsrepository.blob.core.windows.net/msix/NSW-MSIX-Test-Cert.pfx"
+Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\msixcert.pfx"
+$secPassword = ConvertTo-SecureString -String 'Microsoft~1' -Force -AsPlainText
+Import-PfxCertificate -Password $secPassword -FilePath "c:\buildArtifacts\msixcert.pfx" -CertStoreLocation "Cert:\LocalMachine\TrustedPeople"
