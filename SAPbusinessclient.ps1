bb220923b3b296f41564344036e8d1dfc0d6d184
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-03 15:33:06 +0530

    Create SAPbusinessclient.ps1

---
+++ SAPbusinessclient.ps1
@@ -0,0 +1,6 @@
+$uri="https://avdappsrepository.blob.core.windows.net/appbinaries/SAP_ERPBusinessUserClientSuite_2.2.zip"
+Invoke-WebRequest -Uri "$uri" -UseBasicParsing -OutFile "C:\buildArtifacts\SAPuserclient.zip"
+Expand-Archive -LiteralPath C:\buildArtifacts\SAPuserclient.zip -DestinationPath C:\buildArtifacts -Force -Verbose
+cd C:\buildArtifacts\SAP_ERPBusinessUserClientSuite_2.2\Package\x86
+.\SAP_ERPSupportUserClientSuite_2.2_ALL_R1_Install.vbs
+sleep 90
