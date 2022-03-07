9da4fc29473c0639153459a86f96b4c95450ed6c
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-07 13:08:05 +0530

    Update SAPbusinessclient.ps1

--- SAPbusinessclient.ps1
+++ SAPbusinessclient.ps1
@@ -3,4 +3,4 @@ Invoke-WebRequest -Uri "$uri" -UseBasicParsing -OutFile "C:\buildArtifacts\SAPus
 Expand-Archive -LiteralPath C:\buildArtifacts\SAPuserclient.zip -DestinationPath C:\buildArtifacts -Force -Verbose
 cd C:\buildArtifacts\SAP_ERPBusinessUserClientSuite_2.2\Package\x86
 .\SAP_ERPSupportUserClientSuite_2.2_ALL_R1_Install.vbs
-sleep 180
+sleep 360
