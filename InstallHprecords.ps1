2a6fbfbef7ed297debe052f69039245c0d9f7a0a
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 21:33:35 +0530

    Update InstallHprecords.ps1

--- InstallHprecords.ps1
+++ InstallHprecords.ps1
@@ -5,3 +5,4 @@ Expand-Archive -LiteralPath "C:\buildArtifacts\HPE_RecordsManager.zip" -Destinat
 cd C:\buildArtifacts\HPE_RecordsManager_x64_8.30.9365\Package\x64\
 .\HP_RecordsManager_x64_8.3.0.9365_ALL_R2_Install.vbs
 sleep 90
+Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Installed-HPRecords-Manager
