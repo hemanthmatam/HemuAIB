b7d622be1431b6bd5da5a1d63d605189e2bb828f
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 21:40:26 +0530

    Update importcert.ps1

--- importcert.ps1
+++ importcert.ps1
@@ -2,3 +2,4 @@ $uri1="https://avdappsrepository.blob.core.windows.net/msix/NSW-MSIX-Test-Cert.p
 Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\msixcert.pfx"
 $secPassword = ConvertTo-SecureString -String 'Microsoft~1' -Force -AsPlainText
 Import-PfxCertificate -Password $secPassword -FilePath "c:\buildArtifacts\msixcert.pfx" -CertStoreLocation "Cert:\LocalMachine\TrustedPeople"
+Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Imported-MSIX-Certificate
