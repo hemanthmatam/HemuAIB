dea85844f67784c4f96d08e153f44cdaecd18f90
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 20:25:49 +0530

    Create InstallTeambinderoutlook.ps1

---
+++ InstallTeambinderoutlook.ps1
@@ -0,0 +1,6 @@
+$uri1="https://avdappsrepository.blob.core.windows.net/appbinaries/InEight_TeamBinderOutlookIntegration_x64_1.15.7424.zip"
+Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\Teambinderoutlook.zip"
+Expand-Archive -LiteralPath C:\buildArtifacts\Teambinderoutlook.zip -DestinationPath C:\buildArtifacts -Force -Verbose
+cd C:\buildArtifacts\InEight_TeamBinderOutlookIntegration_x64_1.15.7424\Package\x64
+.\QASoftware_TeamBinderOutlookIntegration_x64_1.15.7424_ALL_R1.cmd install
+sleep 180
