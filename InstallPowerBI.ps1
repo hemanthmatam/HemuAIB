1d5e9d53b7f58429e382a525c5e7c6c783fdf7cc
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 21:35:16 +0530

    Update InstallPowerBI.ps1

--- InstallPowerBI.ps1
+++ InstallPowerBI.ps1
@@ -4,3 +4,4 @@ Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\PBID
 cd "c:\buildArtifacts"
 .\PBIDesktopSetup_x64.exe -s ACCEPT_EULA=1
 Start-Sleep -Seconds 90
+Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Installed-PBIDesktopapp
