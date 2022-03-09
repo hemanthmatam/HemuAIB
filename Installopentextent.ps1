bdaeb3a9a4b2e7886908ea4b155fe17fecc798b7
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 21:37:16 +0530

    Update Installopentextent.ps1

--- Installopentextent.ps1
+++ Installopentextent.ps1
@@ -5,3 +5,4 @@ Expand-Archive -LiteralPath C:\buildArtifacts\Opentext20-2.zip -DestinationPath 
 cd C:\buildArtifacts\OpenText_EnterpriseConnectFramework_x64_20.2.0.506\Package\x64
 .\OpenText_EnterpriseConnectFramework_x64_20.2.0.506_ALL_R2_Install.vbs
 sleep 180
+Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Installed-opentext-20-2-version
