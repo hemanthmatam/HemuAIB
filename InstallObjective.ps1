eea7a2e1ae43f824e87d171f131adabe1abeb67c
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 13:10:22 +0530

    Create InstallObjective.ps1

---
+++ InstallObjective.ps1
@@ -0,0 +1,6 @@
+$uri1="https://avdappsrepository.blob.core.windows.net/appbinaries/Objective_Objective_x64_10.6.306.zip"
+Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\Objective10-6.zip"
+Expand-Archive -LiteralPath C:\buildArtifacts\Objective10-6.zip -DestinationPath C:\buildArtifacts -Force -Verbose
+cd C:\buildArtifacts\Objective_Objective_x64_10.6.306\Package\x64
+.\Objective_Objective_x64_10.6.306_ALL_R3_Install.vbs
+sleep 120
