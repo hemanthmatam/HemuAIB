84b8862c495d8a2fe35623c26f28ea750a38a555
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 21:29:42 +0530

    Update Createnewfolder.ps1

--- Createnewfolder.ps1
+++ Createnewfolder.ps1
@@ -1,2 +1,4 @@
 mkdir c:\buildArtifacts
+mkdir c:\buildArtifactslogs
 echo Azure-Image-Builder-Was-Here  > c:\buildArtifacts\azureImageBuilder.txt
+echo AIB-Created-Buildartifactslogs-Folder  > c:\buildArtifactslogs\azureImageBuilder.txt
