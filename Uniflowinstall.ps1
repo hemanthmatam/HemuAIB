7ec9f6cb8ba7b500fbb290ee97d97226105f8af6
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-09 12:02:46 +0530

    Update Uniflowinstall.ps1

--- Uniflowinstall.ps1
+++ Uniflowinstall.ps1
@@ -1,4 +1,4 @@
-$uri5="https://avdappsrepository.blob.core.windows.net/appbinaries/MomSmartClient_x64_transportnp_multi-instance_20220113.msi"
+$uri5="https://avdappsrepository.blob.core.windows.net/appbinaries/MomSmartClient_x64transport_multi-instance_20220222 prod.msi"
 Invoke-WebRequest -Uri "$uri5" -UseBasicParsing -OutFile "c:\buildArtifacts\momsmartclient.msi"
 cd "C:\buildArtifacts"
 .\momsmartclient.msi /quiet
