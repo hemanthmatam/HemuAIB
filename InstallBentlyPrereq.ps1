f1be4efd2ce6cda886d61b30a4c6fe0ae3b97911
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-17 09:49:17 +0530

    Create InstallBentlyPrereq.ps1

---
+++ InstallBentlyPrereq.ps1
@@ -0,0 +1,9 @@
+$uri1="https://avdappsrepository.blob.core.windows.net/appbinaries/Bentley_ProjectWiseExplorer_x64_10.00.3434.zip"
+Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\Bentlyprojectexplorer.zip"
+Expand-Archive -LiteralPath C:\buildArtifacts\Bentlyprojectexplorer.zip -DestinationPath C:\buildArtifacts -Force -Verbose
+
+cd C:\buildArtifacts\Bentley_ProjectWiseExplorer_x64_10.00.3434\Package\x64\
+.\Bentley_ProjectWiseExplorer_x64_10.00.3434_ALL_R1_Install.vbs
+Sleep 150
+
+Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Installed-bently-projectexpl
