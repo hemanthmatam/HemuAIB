9f6a9841de3042243d2da074c5c41e59a98dce8b
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-17 18:17:28 +0530

    Update InstallBentlyPrereq.ps1

--- InstallBentlyPrereq.ps1
+++ InstallBentlyPrereq.ps1
@@ -3,6 +3,6 @@ Invoke-WebRequest -Uri "$uri1" -UseBasicParsing -OutFile "c:\buildArtifacts\Bent
 Expand-Archive -LiteralPath C:\buildArtifacts\Bentlyprojectexplorer.zip -DestinationPath C:\buildArtifacts -Force -Verbose
 cd C:\buildArtifacts\Bentley_ProjectWiseExplorer_x64_10.00.3434\Package\x64\
 .\Bentley_ProjectWiseExplorer_x64_10.00.3434_ALL_R1_Install.vbs
-Sleep 300
+Sleep 240
 
 Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Installed-bently-projectexplprerequsite
