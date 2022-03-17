171746829d09c8a59b6ffcf8da4d34575afae42c
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-17 17:32:04 +0530

    Update InstallBentlyprojectWebsrv.ps1

--- InstallBentlyprojectWebsrv.ps1
+++ InstallBentlyprojectWebsrv.ps1
@@ -3,5 +3,5 @@ Invoke-WebRequest -Uri "$uri" -UseBasicParsing -OutFile "c:\buildArtifacts\Bentl
 Expand-Archive -LiteralPath C:\buildArtifacts\BentlyprojectWebserver.zip -DestinationPath C:\buildArtifacts -Force -Verbose
 cd C:\buildArtifacts\Bentley_ProjectWiseWebServerClientComponents_x64_8.11.11\Package\x86
 .\Bentley_ProjectWiseWebServerClientComponents_8.11.11_ALL_R1_Install.cmd
-sleep 300
+sleep 120
 Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Installed-bently-Webserver
