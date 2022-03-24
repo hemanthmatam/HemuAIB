43bd967e3f44c957bb586a44e3a0ab8e72511799
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-03-24 13:02:45 +0530

    Update InstallAcrobatDc.ps1

--- InstallAcrobatDc.ps1
+++ InstallAcrobatDc.ps1
@@ -1,9 +1,10 @@
-https://avdappsrepository.blob.core.windows.net/appbinaries/Adobe_Acrobat_DC_21.007.20099.zip
-
 $uri15="https://avdappsrepository.blob.core.windows.net/appbinaries/Adobe_Acrobat_DC_21.007.20099.zip"
 Invoke-WebRequest -Uri "$uri15" -UseBasicParsing -OutFile "C:\buildArtifacts\acrobatdc.zip"
 Expand-Archive -LiteralPath "C:\buildArtifacts\acrobatdc.zip" -DestinationPath "C:\buildArtifacts" -Force -Verbose
-cd 'C:\buildArtifacts\Adobe_Acrobat_DC_21.007.20099\Source\Acrobat DC for AVD_en_US_WIN_64\Acrobat DC for AVD\Build'
+
+Expand-Archive -LiteralPath 'C:\buildArtifacts\Adobe_Acrobat_DC_21.007.20099\Source\Acrobat DC for AVD_en_US_WIN_64.zip' -DestinationPath "C:\buildArtifacts\Adobe_Acrobat_DC_21.007.20099\Source" -Force
+cd 'C:\buildArtifacts\Adobe_Acrobat_DC_21.007.20099\Source\Acrobat DC for AVD\Build'
 .\setup.exe --silent
-sleep 180
+sleep 300
+
 Add-Content -Path C:\buildArtifactslogs\azureImageBuilder.txt -Value `nAIB-Installed-AcrobatDC
