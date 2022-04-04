242c6b701d766f9d8409ccda40175d438a9f3c44
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-04-04 18:48:57 +0530

    Update DisableAutoupdate.ps1

--- DisableAutoupdate.ps1
+++ DisableAutoupdate.ps1
@@ -2,3 +2,8 @@ $reg_path = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
 if (-Not (Test-Path $reg_path)) { New-Item $reg_path -Force }
 Set-ItemProperty $reg_path -Name NoAutoUpdate -Value 1
 Set-ItemProperty $reg_path -Name AUOptions -Value 3
+Set-Culture en-AU
+Set-WinSystemLocale en-AU
+Set-WinHomeLocation -GeoId 12
+Set-WinUserLanguageList en-AU -force
+Set-TimeZone -Id "AUS Eastern Standard Time"
