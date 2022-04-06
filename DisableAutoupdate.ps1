0b813d5f00bc52043af771b680c32670dce4714c
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-04-06 14:40:49 +0530

    Update DisableAutoupdate.ps1

--- DisableAutoupdate.ps1
+++ DisableAutoupdate.ps1
@@ -2,8 +2,10 @@ $reg_path = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
 if (-Not (Test-Path $reg_path)) { New-Item $reg_path -Force }
 Set-ItemProperty $reg_path -Name NoAutoUpdate -Value 1
 Set-ItemProperty $reg_path -Name AUOptions -Value 3
-Set-Culture en-AU
-Set-WinSystemLocale en-AU
-Set-WinHomeLocation -GeoId 12
-Set-WinUserLanguageList en-AU -force
-Set-TimeZone -Id "AUS Eastern Standard Time"
+sleep 5
+$uri="https://avdappsrepository.blob.core.windows.net/appbinaries/AURegion.zip"
+Invoke-WebRequest -Uri "$uri" -UseBasicParsing -OutFile "C:\buildArtifacts\auregion.zip"
+Expand-Archive -LiteralPath "C:\buildArtifacts\auregion.zip" -DestinationPath "C:\buildArtifacts" -Force -Verbose
+cd C:\buildArtifacts
+.\Apply-RegionSettings.ps1
+Sleep 10
