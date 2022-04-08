f532a0ab93bc8d697e9d86ec95f58a6bee8e3fe8
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-04-08 17:28:43 +0530

    Update InstallAcrobatDc.ps1

--- InstallAcrobatDc.ps1
+++ InstallAcrobatDc.ps1
@@ -1,5 +1,5 @@
-$uri="https://avdappsrepository.blob.core.windows.net/appbinaries/Acrobat DC for AVD_en_US_WIN_64.zip"
-Invoke-WebRequest -Uri "$uri" -UseBasicParsing -OutFile "C:\buildArtifacts\acrobatdc.zip"
+$link="https://avdappsrepository.blob.core.windows.net/appbinaries/Acrobat DC for AVD_en_US_WIN_64.zip"
+Invoke-WebRequest -Uri "$link" -UseBasicParsing -OutFile "C:\buildArtifacts\acrobatdc.zip"
 Expand-Archive -LiteralPath "C:\buildArtifacts\acrobatdc.zip" -DestinationPath "C:\buildArtifacts" -Force -Verbose
 cd 'C:\buildArtifacts\Acrobat DC for AVD\Build'
 .\setup.exe --silent
