71e1325fc5a16376b19906e2d5a8ff04042b0209
Hemanth Matam <95575538+hemanthmatam@users.noreply.github.com>
2022-04-11 11:12:20 +0530

    Update InstallAcrobatDc.ps1

--- InstallAcrobatDc.ps1
+++ InstallAcrobatDc.ps1
@@ -2,6 +2,7 @@ New-Item -Path c:\ -ItemType Directory -Name "AcrobatDC"
 $link="https://avdappsrepository.blob.core.windows.net/appbinaries/Acrobat DC for AVD_en_US_WIN_64.zip"
 Invoke-WebRequest -Uri "$link" -UseBasicParsing -OutFile "C:\AcrobatDC\acrobatdc.zip"
 Expand-Archive -LiteralPath "C:\AcrobatDC\acrobatdc.zip" -DestinationPath "C:\AcrobatDC" -Force -Verbose
+sleep 30
 cd "C:\AcrobatDC\Acrobat DC for AVD\Build"
 .\setup.exe --silent
 sleep 300
